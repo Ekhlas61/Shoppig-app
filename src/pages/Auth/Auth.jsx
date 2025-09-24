c72e351e9e6d7fc64cfadacdae0e35cab96390bb
Ekhlas61 <ekhlasabdulmelik@gmail.com>
2025-09-24 17:19:47 +0300

    authentication functionality

--- src/pages/Auth/Auth.jsx
+++ src/pages/Auth/Auth.jsx
@@ -1,7 +1,53 @@
-import React from 'react'
+import React, { useState,useContext } from 'react'
 import classes from './Signup.module.css'
 import { Link } from 'react-router-dom'
+import {auth} from '../../Utility/firebase'
+import{signInWithEmailAndPassword,createUserWithEmailAndPassword} from 'firebase/auth'
+import { DataContext } from '../../Components/DataProvider/DataContext'
+import { Type } from '../../Utility/action.type'
+
+
 function Auth() {
+const [email,setEmail]=useState('');
+const [password,setPassword]=useState('')
+// const [error,setError]=useState('')
+
+const[{user},dispatch]=useContext(DataContext)
+
+console.log(user)
+
+const authHandler = async(e) => {
+    e.preventDefault();
+    console.log(e.target.name);
+    if(e.target.name == "signin") {
+    // firebase auth
+    signInWithEmailAndPassword(auth, email, password).then((userInfo)=>{
+      
+dispatch({
+  type:Type.SET_USER,
+  user:userInfo.user
+})
+
+    }).catch((err)=>{
+      console.log(err)
+    })
+
+    }else{
+      createUserWithEmailAndPassword(auth,email,password).then((userInfo)=>{
+        
+dispatch({
+  type: Type.SET_USER,
+  user: userInfo.user,
+});
+
+      }).catch((err)=>{
+        console.log(err)
+      })
+    }
+};
+
+// console.log(password_email);
+
   return (
     <section className={classes.login}>
       {/* logo */}
@@ -18,13 +64,14 @@ function Auth() {
         <form action="">
           <div>
             <label htmlFor="email">Email</label>
-            <input type="email" id="email" />
+            <input value={email} onChange={(e)=>setEmail(e.target.value)} type="email" id="email" />
           </div>
           <div>
             <label htmlFor="password">Password</label>
-            <input type="password" id="password" />
+            <input value={password} onChange={(e)=>setPassword(e.target.value)} type="password" id="password" />
           </div>
-          <button className={classes.login__signInButton}>Sign In</button>
+          <button type='submit' onClick={authHandler}
+          name='signin' className={classes.login__signInButton}>Sign In</button>
         </form>
 
         {/* agreement */}
@@ -35,7 +82,8 @@ function Auth() {
         </p>
 
         {/* create account btn */}
-        <button className={classes.login__registerButton}>
+        <button type='submit' 
+        name='signup' onClick={authHandler}   className={classes.login__registerButton}>
           Create your Amazon Account
         </button>
       </div>
