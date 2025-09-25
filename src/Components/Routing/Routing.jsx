f19514ed0737bf593b03fe4f0be8983b2cea6eeb
Ekhlas61 <ekhlasabdulmelik@gmail.com>
2025-09-25 15:54:30 +0300

    payment page UI

--- src/Components/Routing/Routing.jsx
+++ src/Components/Routing/Routing.jsx
@@ -7,6 +7,13 @@ import Orders from '../../pages/Orders/Orders'
 import Cart from '../../pages/Cart/Cart'
 import Results from '../../pages/Results/Results'
 import ProductDetail from '../../pages/ProductDetail/ProductDetail'
+import { Elements } from "@stripe/react-stripe-js";
+
+import { loadStripe } from "@stripe/stripe-js";
+
+const stripePromise = loadStripe(
+  "pk_test_51SAwSoR8xKgDpvm8r52eEqHDepx8NG33LytJi2rkZZuCJgtAUZgEMkjpJbKv1DGFO7Uj1hir3jczYG884A5mUauo00bvjjtCEf"
+);
 
 function Routing() {
   return (
@@ -14,14 +21,21 @@ function Routing() {
       <Routes>
         <Route path="/" element={<Landing />} />
         <Route path="/auth" element={<Auth />} />
-        <Route path="/payments" element={<Payment />} />
+        <Route
+          path="/payments"
+          element={
+            <Elements stripe={stripePromise}>
+              <Payment />
+            </Elements>
+          }
+        />
         <Route path="/orders" element={<Orders />} />
-        <Route path='/category/:categoryName' element={<Results/>}/>
-        <Route path='/products/:productId' element={<ProductDetail/>}/>
+        <Route path="/category/:categoryName" element={<Results />} />
+        <Route path="/products/:productId" element={<ProductDetail />} />
         <Route path="/cart" element={<Cart />} />
       </Routes>
     </Router>
-  )
+  );
 }
 
 export default Routing
